204e13c5e08419cdcde0c8c3e95e03cd9c014e51
Anil Tellbuescher <anil@tellbuescher.online>
2025-01-13 09:54:44 +0100

    add core structure of function

---
+++ R/here_S.R
@@ -0,0 +1,42 @@
+server_employee_folder <- "S:\010 SLOŽKY ZAMĚSTNANCU - FOLDERS OF EMPLOYEES"
+
+name <- "TELLBUSCHER AXEL"
+
+folder <- "TELLBUSCHER - READ WRITE FREE"
+
+filepath <- ""
+
+# 1. pre-define inputs
+# 2. based on name and folder variable, reformat the folder name
+
+here_S <- function(file_path,
+                   name,
+                   folder = c("r", "rw"),
+                   server_path = "S:\010 SLOŽKY ZAMĚSTNANCU - FOLDERS OF EMPLOYEES") {
+
+  # Make sure all name inputs work
+  name <- toupper(name)
+
+
+  # Create folder_path, based on the last name and the selected folder
+  last_name <- strsplit(name, " ")[[1]][1]
+
+  if(folder == "r") {
+    folder_name <- "READ-ONLY"
+  } else if (folder == "rw") {
+    folder_name <- "READ-WRITE FREE"
+  } else {
+    warning("Select the folder!\n
+            'r'   = READ-ONLY\n
+            'rw'  = READ-WRITE")
+  }
+
+  folder_path <- paste(last_name, folder_name)
+
+
+  # Create full path
+  full_path <- paste(server_path, name, folder_path, file_path,
+                     sep = "\\")
+
+  return(full_path)
+}
