af8bcd8163ef17956991da8d7f8d838cfea1cf99
Anil Tellbuescher <anil@tellbuescher.online>
2025-02-05 19:31:12 +0100

    debug function

--- R/read_aas.R
+++ R/read_aas.R
@@ -1,13 +1,10 @@
-#' Import data from Shimadzu F-AAS
+#'  Import data from Shimadzu F-AAS
 #'
 #'  Function that imports either a basic or a detailed data report file
 #'  from the Shimadzu AA-7000 Flame-Atomic absorption spectroscope
 #'  located in the small lab at ZR.
 #'
 #'  @param file_path character; path to the file to load
-#'  @param detailed logical; indicating whether the file to be loaded is a basic
-#'  report (only summarized results) or a detailed report (including data from
-#'  individual injections).
 #'  @param skip_lines numeric; number of lines to be skipped when reading the
 #'  file. The value depends on how many lines the initial comment at the
 #'  beginning of a measurement file has.
@@ -16,19 +13,13 @@
 #'
 #'  @author Anil Axel Tellbüscher
 #'
+#'  @importFrom readr read_delim
+#'
 #'  @export
 read_aas <- function(
-
-  # Define the path to the file to load
   file_path,
-
-  # Define whether the file to be loaded is
-  # "detailed": including data from each individual sample injection
-  # "standard": including summarized results
-  detailed = FALSE,
-
   skip_lines = 2
-) {
+  ) {
 
   aas_data <- readr::read_delim(
     file = file_path,
